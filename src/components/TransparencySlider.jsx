3e67057cdd1de07fa50e51e275438e1e878e33cf
dreamxinxcode <dreamxinxcode@gmail.com>
2021-01-31 03:06:26 -0800

    add handleChange handlers

--- src/components/TransparencySlider.jsx
+++ src/components/TransparencySlider.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './TransparencySlider.scss';
 
 function TransparencySlider() {
+  const [transparency, setTransparency] = useState(0.50)
 
+  const handleChange = (event) => {
+    setTransparency(event.target.value);
+  }
 
   return (
     <div>
-      <label htmlFor="blur">Transparency</label>
-      <input type="range" id="transparency" name="transparency" min="0.00" max="1.00"></input>
+      <label htmlFor="transparency">Transparency</label>
+      <span>{transparency}</span>
+      <input onChange={handleChange} type="range" id="transparency" name="transparency" min="0.00" max="1.00" step='0.01'></input>
     </div>
   )
 }
