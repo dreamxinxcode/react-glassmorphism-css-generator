0677f2286be7e159897827ff6385e1a7ea01f79e
dreamxinxcode <dreamxinxcode@gmail.com>
2021-02-04 02:23:32 -0800

    fix line height

--- src/components/settings/Output.jsx
+++ src/components/settings/Output.jsx
@@ -10,10 +10,10 @@ function Output() {
     <div id='output'>
       <Clipboard />
       <div id="code">
-        <span id='background'>background: rgba({settings.color}, {settings.transparency} );</span>
-        <span id='box-shadow'>box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );</span>
-        <span id='backdrop-filter'>backdrop-filter: blur( {settings.blur}px );</span>
-        <span id='wk-backdrop-filter'>-webkit-backdrop-filter: blur( {settings.blur}px );</span>          
+        <span className='code-line' id='background'>background: rgba({settings.color}, {settings.transparency} );</span>
+        <span className='code-line' id='box-shadow'>box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );</span>
+        <span className='code-line' id='backdrop-filter'>backdrop-filter: blur( {settings.blur}px );</span>
+        <span className='code-line' id='wk-backdrop-filter'>-webkit-backdrop-filter: blur( {settings.blur}px );</span>          
       </div>
     </div>
   )
